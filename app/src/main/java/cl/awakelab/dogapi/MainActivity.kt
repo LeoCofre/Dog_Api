7287410f245af283fcef9c9d4d6e4cbca714486a
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-08-07 19:58:35 -0400

    Inicio proyecto, se agregan dependencia y se activa viewBinding + permiso internet

---
+++ app/src/main/java/cl/awakelab/dogapi/MainActivity.kt
@@ -0,0 +1,29 @@
+package cl.awakelab.dogapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+[] 2 pantallas: Listado y detalle
+[] Permiso de internet
+[] ViewBinding
+[] Dependencias: Coil, Room, Retrofit, navigation, viewModel
+[] navigation
+[] Listado: RecyclerView + ViewHolder + Adapter
+[] Detalle: RecyclerView + ViewHolder + Adapter
+[] Repositorio
+[] Remoto:
+    [] Dataclass
+    [] API (interfaz)
+    [] Retrofit
+[] Local:
+    [] Database
+    [] Entity
+    [] DAO
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
